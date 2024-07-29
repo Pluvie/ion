a843332a7208940a1a3bcd8ae5550c401b4fc876
Pluvie <francesco.ballardin@gmail.com>
2024-07-29 09:59:25 +0200

    improved arrayof and io_exhausted macros

--- src/ion/headers/macros/io_exhausted.h
+++ src/ion/headers/macros/io_exhausted.h
@@ -1,4 +1,4 @@
 /**
  * Checks if the io cursor has reached the io length. */
 #define io_exhausted(io) \
-  ((io)->cursor == (io)->length)
+  ((io)->type != IO_TYPE_SOCK ? ((io)->cursor == (io)->length) : false)
