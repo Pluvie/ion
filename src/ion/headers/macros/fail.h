7bfff512052adea5e82d3b330a5df18d130f9e6e
Pluvie <francesco.ballardin@gmail.com>
2024-10-02 11:08:44 +0200

    protected against error undefined behaviour in fail macro

--- src/ion/headers/macros/fail.h
+++ src/ion/headers/macros/fail.h
@@ -1,9 +1,10 @@
 /**
  * Sets a failure with the given printf-like *format* string. */
-#define fail(format, ...) {             \
-  error.occurred = true;                \
-  error.length = snprintf(              \
-    error.message,                      \
-    sizeof(error.message),              \
-    format __VA_OPT__(, __VA_ARGS__));  \
+#define fail(format, ...) {                         \
+  error.occurred = true;                            \
+  char message[sizeof(error.message)] = { 0 };      \
+  error.length = snprintf(                          \
+    message, sizeof(message),                       \
+    format __VA_OPT__(, __VA_ARGS__));              \
+  memcpy(error.message, message, sizeof(message));  \
 }
