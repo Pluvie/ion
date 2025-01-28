8f438fd150887a8f214d6bbd471947605a80e48b
Pluvie <francesco.ballardin@gmail.com>
2025-01-28 10:27:30 +0100

    added skarupke flat_hash_map to prf

--- prf/map/skarupke/flat_hash_map.cpp
+++ prf/map/skarupke/flat_hash_map.cpp
@@ -3,47 +3,19 @@
 #include "flat_hash_map.hpp"
 #include <string>
 
+void insert () {
+  ska::flat_hash_map<int, int> map = {};
+
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < 10000; j++) {
+      map[j] = i;
+    }
+  }
+
+  std::cout << "Done: " << map[999] << "\n";
+}
+
 int main() {
-    // Create an unordered_map of three strings (that map to strings)
-    std::unordered_map<std::string, std::string> u = {
-        {"RED", "#FF0000"},
-        {"GREEN", "#00FF00"},
-        {"BLUE", "#0000FF"}
-    };
-
-    // Helper lambda function to print key-value pairs
-    auto print_key_value = [](const auto& key, const auto& value) {
-        std::cout << "Key:[" << key << "] Value:[" << value << "]\n";
-    };
-
-    // Iterate and print key-value pairs of unordered_map, being explicit with their types
-    std::cout << "Iterate and print key-value pairs of unordered_map, being\n"
-                 "explicit with their types:\n";
-    for (const std::pair<std::string, std::string>& n : u)
-        print_key_value(n.first, n.second);
-
-    // Iterate and print key-value pairs using C++17 structured binding
-    std::cout << "\nIterate and print key-value pairs using C++17 structured binding:\n";
-    for (const auto& [key, value] : u)
-        print_key_value(key, value);
-
-    // Add two new entries to the unordered_map
-    u["BLACK"] = "#000000";
-    u["WHITE"] = "#FFFFFF";
-
-    // Output values by key
-    std::cout << "\nOutput values by key:\n"
-                 "The HEX of color RED is:[" << u["RED"] << "]\n"
-                 "The HEX of color BLACK is:[" << u["BLACK"] << "]\n\n";
-
-    // Use operator[] with non-existent key to insert a new key-value pair
-    print_key_value("new_key", u["new_key"]);
-
-    // Iterate and print key-value pairs, using `auto`
-    std::cout << "\nIterate and print key-value pairs, using `auto`;\n"
-                 "new_key is now one of the keys in the map:\n";
-    for (const auto& n : u)
-        print_key_value(n.first, n.second);
-
-    return 0;
+  insert();
+  return 0;
 }
