c4e1a38922b1176f3cc8298d1ab2272a69b8c7e3
Pluvie <francesco.ballardin@gmail.com>
2025-01-30 09:48:48 +0100

    added MSG_WAITALL flag in io read socket

--- src/ion/functions/io_read_socket.c
+++ src/ion/functions/io_read_socket.c
@@ -11,7 +11,7 @@ static inline void* io_read_socket (
   reader->cursor = 0;
   reader->data = memory_alloc(reader->allocator, amount);
 
-  i32 recv_output = recv(reader->descriptor, reader->data, amount, 0);
+  i32 recv_output = recv(reader->descriptor, reader->data, amount, MSG_WAITALL);
 
   if (unlikely(recv_output < 0)) {
     fail("io: error while reading from socket: %s", strerror(errno));
