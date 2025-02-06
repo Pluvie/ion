b1970d9105f5ee8b776fa52c4af6e844d0f91291
Pluvie <francesco.ballardin@gmail.com>
2025-02-06 11:08:46 +0100

    added documentation on memory and buffer structs

--- src/ion/structs/memory.h
+++ src/ion/structs/memory.h
@@ -1,7 +1,7 @@
 struct memory;
 /**
- * The #struct memory is a memory abstraction to simplify allocation and deallocation
- * of many objects.
+ * The `struct memory` is a memory abstraction, much like the `struct buffer`, to
+ * simplify allocation and deallocation of many objects.
  *
  * This struct takes inspiration from the [arena allocation](
  * https://www.rfleury.com/p/untangling-lifetimes-the-arena-allocator). It works by
@@ -12,11 +12,16 @@ struct memory;
  * The memory shall automatically grow to fit as many objects as necessary. This struct
  * exposes the following functions to allocate memory:
  *
- * - #memory_alloc : allocates the given amount of memory in the allocator
- * - #memory_alloc_zero : allocates the given amount of memory in the allocator, and
- *     sets its content to `0`.
+ * - `memory_alloc` : allocates the given amount of memory in the allocator and returns
+ *     a pointer to this memory.
+ * - `memory_alloc_zero` : allocates the given amount of memory in the allocator, sets
+ *     its content to `0`, and returns a pointer to this memory.
  *
- * Memory relase is done all at once, by calling the #memory_release function. */
+ * Memory relase is done all at once, by calling the `memory_release` function.
+ *
+ * The `struct memory` differs from the `struct buffer` because memory allocation is
+ * not linear and is prone to fragmentation. However, its main benefit is that returned
+ * pointers after allocations are guaranteed to be always valid. */
 struct memory {
   u64 capacity;
   u64 position;
