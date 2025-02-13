d0a5103305cdee99166ce1f8fdcf05b1cf328516
Pluvie <francesco.ballardin@gmail.com>
2025-02-13 16:47:23 +0100

    turns out, no twiddling: just a wrong print

--- src/ion/macros/error_print.h
+++ src/ion/macros/error_print.h
@@ -2,7 +2,7 @@
  * Prints the error. */
 #define error_print() {                                   \
   print_c(PRINT_COLOR_RED, "Error: %s", error.message);   \
-  for (i32 i = error.trace_count; i >= 0; i--)            \
+  for (i32 i = error.trace_count - 1; i >= 0; i--)        \
     print_c(PRINT_COLOR_RED, "[%s:%li]",                  \
       error.trace[i].file, error.trace[i].line);          \
 }
