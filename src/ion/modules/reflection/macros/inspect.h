b296a4f714e40a569e839aa0f8dd0ca0b4863229
Pluvie <francesco.ballardin@gmail.com>
2025-03-21 06:51:09 +0100

    pulled off a mega trick with inspect macro

--- src/ion/modules/reflection/macros/inspect.h
+++ src/ion/modules/reflection/macros/inspect.h
@@ -1,4 +1,8 @@
 /**
  * Used to inspect an object with reflection. */
-#define inspect(obj, rfx) \
-  reflection_inspect(rfx, &s(#obj), obj)
+#define inspect(obj, rfx, ...)        \
+  reflection_inspect(rfx,             \
+    (__VA_OPT__(1+)0) ?               \
+      __VA_OPT__((__VA_ARGS__)+)0 :   \
+      &s(#obj),                       \
+    obj)
