214c61fdd4628e4ffc38a1f7019d048d27c98499
Pluvie <francesco.ballardin@gmail.com>
2025-06-20 15:36:43 +0200

    the symbol inflector

--- bin/ion.py
+++ bin/ion.py
@@ -3,29 +3,34 @@
 #
 # !!! A FREAKING NAME MANGLER !!!
 #
+# Or, to say it more properly, a "symbol inflector".
+#
 # Yeah, with just this little functionality a world of possibilities opens before
 # our heart-shaped developer eyes.
 #
 # This script basically reads a post-processed C source (i.e. with all macros expanded)
 # and substitutes each occurrence of tokens like `map<int, string>` with the appropriate
-# mangled name.
+# inflected name.
 #
 # This allows us to build type containers in pure C, and with the introduction of C11
 # `_Generic` expression, a wealth of unprecedented possibilities are now open for the
-# die hard C developers.
+# die hard C developers like us.
+#
+# Oh, and next step, we all know what it is: implement this script in C, of course!
+# Joking, Python has definitely its uses. This is one of 'em.
 
 import sys
 import re
 
 
 # `s` is a type container like: `map<int, struct user*>`
-def mangle(s):
+def inflect(s):
   # `_sym` becomes `map`
   # `_type` contains `<int, struct user*>`
   _sym = s[:s.find("<")]
   _type = s[s.find("<"):]
 
-  # We may now apply the appropriate substitutions in order to mangle the type.
+  # We may now apply the appropriate substitutions in order to inflect the type.
   return (_sym + "_" + _type.
       replace(",", "__").
       replace("struct ", "struct_").
@@ -44,5 +49,5 @@ pattern = re.compile(r'([a-z_]+<[a-z0-9_,\*\[\]\(\) ]+>)')
 content = sys.stdin.read()
 # Increment the range depending on how many nested data structures you have.
 for i in range(2):
-  content = re.sub(pattern, lambda m: mangle(m.group()), content)
+  content = re.sub(pattern, lambda m: inflect(m.group()), content)
 print(content, end="")
