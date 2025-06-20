fc2be4bde16d4ab1ddb2563737fb89dbe074e1bf
Pluvie <francesco.ballardin@gmail.com>
2025-06-20 21:35:00 +0200

    done list push spec

--- src/spec/list_push.c
+++ src/spec/list_push.c
@@ -3,98 +3,142 @@ spec( list_push ) {
   argument(list<int>* list);
   argument(int element);
 
-  precondition("a valid list");
-    #define valid_list_precondition \
-      list = list_alloc(int, 16, spec_allocator);
-
   precondition("a valid element")
     #define valid_element_precondition \
       element = 7;
 
-  when("the list length is inferior to its capacity - 1") {
-    #define list_length_condition \
-      list->length = 2; \
-      verify(list->length < list->capacity - 1);
+  when("the list is stack allocated") {
+    #define stack_allocated_list_condition \
+      list<int> stack_list = list_init(int, 16); \
+      list = &stack_list;
 
-    apply(valid_list_precondition);
-    apply(valid_element_precondition);
-    apply(list_length_condition);
+    when("the list length is inferior to its capacity") {
+      #define list_length_condition \
+        list->length = list->capacity - 3; \
+        int original_list_length = list->length; \
+        int original_list_capacity = list->capacity;
 
-    bool result = list_push(list, element);
+      apply(valid_element_precondition);
+      apply(stack_allocated_list_condition);
+      apply(list_length_condition);
 
-    must("return true");
-      verify(result == true);
+      bool result = list_push(list, element);
 
-    must("increase the list length by 1");
-      verify(list->length == 3);
+      must("return true");
+        verify(result == true);
 
-    must("copy the element at the position equal to the list length");
-      verify(*(list->data + 2) == element);
+      must("increase the list length by 1");
+        verify(list->length == original_list_length + 1);
 
-    must("leave the list capacity unchanged");
-      verify(list->capacity == 16);
+      must("copy the element at the position equal to the list length");
+        verify(*(list->data + original_list_length) == element);
 
-    success();
+      must("leave the list capacity unchanged");
+        verify(list->capacity == original_list_capacity);
 
-    #undef list_length_condition
+      success();
+
+      #undef list_length_condition
+    }
+
+    when("the list length is equal to its capacity") {
+      #define list_length_condition \
+        list->length = list->capacity; \
+        int original_list_length = list->length; \
+        int original_list_capacity = list->capacity;
+
+      apply(valid_element_precondition);
+      apply(stack_allocated_list_condition);
+      apply(list_length_condition);
+
+      bool result = list_push(list, element);
+
+      must("return false");
+        verify(result == false);
+
+      must("not copy the element on the list");
+        verify(*(list->data + original_list_length - 1) == 0);
+
+      must("leave the list length and capacity unchanged");
+        verify(list->length == original_list_length);
+        verify(list->capacity == original_list_capacity);
+
+
+      success();
+
+      #undef list_length_condition
+    }
+
+    #undef stack_allocated_list_condition
   }
 
-  when("the list length is equal to its capacity - 1") {
-    #define list_length_condition \
-      list->length = 15; \
-      verify(list->length == list->capacity - 1);
-      int* original_list_data = list->data;
+  when("the list is heap allocated") {
+    #define heap_allocated_list_condition \
+      list = list_alloc(int, 16, spec_allocator);
 
-    apply(valid_list_precondition);
-    apply(valid_element_precondition);
-    apply(list_length_condition);
+    when("the list length is inferior to its capacity") {
+      #define list_length_condition \
+        list->length = list->capacity - 3; \
+        int original_list_length = list->length; \
+        int original_list_capacity = list->capacity;
 
-    bool result = list_push(list, element);
+      apply(valid_element_precondition);
+      apply(heap_allocated_list_condition);
+      apply(list_length_condition);
 
-    must("return true");
-      verify(result == true);
+      bool result = list_push(list, element);
 
-    must("increase the list length by 1");
-      verify(list->length == 16);
+      must("return true");
+        verify(result == true);
 
-    must("copy the element at the position equal to the list length");
-      verify(*(list->data + 15) == element);
+      must("increase the list length by 1");
+        verify(list->length == original_list_length + 1);
 
-    must("double the list capacity");
-      verify(list->capacity == 16);
+      must("copy the element at the position equal to the list length");
+        verify(*(list->data + original_list_length) == element);
 
-    must("reallocate the list with the doubled capacity");
-      verify(list->data != original_list_data);
+      must("leave the list capacity unchanged");
+        verify(list->capacity == original_list_capacity);
 
-    success();
+      success();
 
-    #undef list_length_condition
-  }
+      #undef list_length_condition
+    }
 
-  when("the list does not have an allocator") {
-    #define list_allocator_condition \
-      list->allocator = NULL; \
-      list->length = 5;
+    when("the list length is equal to its capacity") {
+      #define list_length_condition \
+        list->length = list->capacity; \
+        int original_list_length = list->length; \
+        int original_list_capacity = list->capacity; \
+        int* original_list_data = list->data;
 
-    apply(valid_list_precondition);
-    apply(valid_element_precondition);
-    apply(list_allocator_condition);
+      apply(valid_element_precondition);
+      apply(heap_allocated_list_condition);
+      apply(list_length_condition);
 
-    bool result = list_push(list, element);
+      bool result = list_push(list, element);
 
-    must("return false");
-      verify(result == true);
+      must("return true");
+        verify(result == true);
 
-    must("not copy the element on the list");
-      verify(*(list->data + 5) == 0);
+      must("increase the list length by 1");
+        verify(list->length == original_list_length + 1);
 
-    must("leave the list length and capacity unchanged");
-      verify(list->length == 5);
-      verify(list->capacity == 16);
+      must("copy the element at the position equal to the list length");
+        verify(*(list->data + original_list_length) == element);
 
-    success();
+      must("double the list capacity");
+        verify(list->capacity == 2*original_list_capacity);
 
-    #undef list_allocator_condition
+      must("reallocate the list with the doubled capacity");
+        verify(list->data != original_list_data);
+
+      success();
+
+      #undef list_length_condition
+    }
+
+    #undef heap_allocated_list_condition
   }
 
   #undef valid_type_precondition
