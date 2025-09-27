e45fd3c715f0e7123210de08bdfa37860106f4f0
Pluvie <francesco.ballardin@gmail.com>
2025-09-27 17:14:17 +0200

    changed bit count functions to use standard macro

--- src/ion/functions/bit_count_leading_ones.c
+++ src/ion/functions/bit_count_leading_ones.c
@@ -2,17 +2,21 @@ unsigned int bit_count_leading_ones (
     unsigned int value
 )
 {
+/*
+  If C standard is C23 and up, the stdc_leading_* functions can be used.
+  If not, resorts to using the compiler builtins.
+*/
 #if   INT_BIT_WIDTH == 64
-  #if __has_include(<stdbit.h>)
-    return stdc_leading_ones_ull(value);
-  #else
+  #if __STDC_VERSION__ <= 201711L   /* C17 */
     return __builtin_popcountll(value);
+  #else
+    return stdc_leading_ones_ull(value);
   #endif
 #elif INT_BIT_WIDTH == 32
-  #if __has_include(<stdbit.h>)
-    return stdc_leading_ones_ul(value);
-  #else
+  #if __STDC_VERSION__ <= 201711L   /* C17 */
     return __builtin_popcountl(value);
+  #else
+    return stdc_leading_ones_ul(value);
   #endif
 #endif
 }
--- src/ion/functions/bit_count_leading_zeros.c
+++ src/ion/functions/bit_count_leading_zeros.c
@@ -2,17 +2,21 @@ unsigned int bit_count_leading_zeros (
     unsigned int value
 )
 {
+/*
+  If C standard is C23 and up, the stdc_leading_* functions can be used.
+  If not, resorts to using the compiler builtins.
+*/
 #if   INT_BIT_WIDTH == 64
-  #if __has_include(<stdbit.h>)
-    return stdc_leading_zeros_ull(value);
-  #else
+  #if __STDC_VERSION__ <= 201711L   /* C17 */
     return __builtin_clzll(value);
+  #else
+    return stdc_leading_zeros_ull(value);
   #endif
 #elif INT_BIT_WIDTH == 32
-  #if __has_include(<stdbit.h>)
-    return stdc_leading_zeros_ul(value);
-  #else
+  #if __STDC_VERSION__ <= 201711L   /* C17 */
     return __builtin_clzl(value);
+  #else
+    return stdc_leading_zeros_ul(value);
   #endif
 #endif
 }
