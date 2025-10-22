bf3682bdb841a69a00b68f50b4aaef48dcf2b4db
Pluvie <francesco.ballardin@gmail.com>
2025-10-22 03:01:38 +0200

    remove redundant definition

--- src/ion/architecture/x64.h
+++ src/ion/architecture/x64.h
@@ -5,6 +5,3 @@
 #define INT_BIT_WIDTH   64
 #define INT_MAXNUM      "9223372036854775807"   /* 2^63 - 1 */
 #define INT_FORMAT      "%l"
-
-typedef int             native_int_t;           /* 32 bit */
-#define widest_int_t    long int                /* 64 bit */
